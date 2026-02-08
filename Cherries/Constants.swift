e29b978ee7e79afe5331de6e215986be6d85ef51
Ziyang <zliu63@illinois.edu>
2026-02-08 14:05:35 -0500

    fix bug

--- Cherries/Constants.swift
+++ Cherries/Constants.swift
@@ -4,5 +4,6 @@ enum Constants {
     enum API {
         // TODO: Update this to your production backend URL before release
         static let baseURL = "https://cherriesservice-production.up.railway.app/api/v1"
+        static let wsBaseURL = "wss://cherriesservice-production.up.railway.app/api/v1"
     }
 }
